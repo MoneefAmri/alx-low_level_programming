8965b7fce5e6a36045f428d8860e4a7fad69231e
Moneef Amri <68125429+MoneefAmri@users.noreply.github.com>
2023-09-12 02:50:27 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,32 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-<<<<<<< HEAD
-/*
- *
- * * main - Entry point
- *
- * *Return: Always 1 (Success)
- */
-
-=======
 /**
 * main - Entry point
 *
 * Return: Always 1 (Success)
 */
->>>>>>> a4b81358360efe811fd85d72ff50b4ddeae0b770
 int main(void)
 {
 	write(2,
-<<<<<<< HEAD
-
-				        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-		        return (1);
-
-=======
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
->>>>>>> a4b81358360efe811fd85d72ff50b4ddeae0b770
+  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+  return (1);
 }
