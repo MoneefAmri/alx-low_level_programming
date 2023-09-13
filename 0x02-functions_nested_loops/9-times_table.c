096e98cf07f04863454ee80a41769430125a61dc
Moneef Amri <68125429+MoneefAmri@users.noreply.github.com>
2023-09-13 03:04:21 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 			if (j == 0)
 			{
 				_putchar(k + '0');
-
+			}
 				if (k < 10 && j != 0)
 				{
 					_putchar(',');
@@ -30,8 +30,7 @@ void times_table(void)
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
-				_putchar('\n');
 			}
+			_putchar('\n');
 		}
-	}
 }
