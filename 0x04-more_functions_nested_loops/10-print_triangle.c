889ca18d00d6f79b5cb4615d2202b3cae4157017
Moneef Amri <68125429+MoneefAmri@users.noreply.github.com>
2023-09-15 03:28:24 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -20,10 +20,12 @@ void print_triangle(int size)
 			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
-				for (j = 1; j <= i; j++)
-				{
-					_putchar('#');
-					_putchar('\n');
-				}
 			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
