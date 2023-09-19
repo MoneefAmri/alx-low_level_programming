8c1738ba0ab2932e22d006431a01dfaa1dd5e3e2
Moneef Amri <68125429+MoneefAmri@users.noreply.github.com>
2023-09-20 00:22:36 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,37 +11,21 @@
  *     */
 
 void print_rev(char *s)
-
 {
+	int longi = 0;
+	int o;
 
-	        int longi = 0;
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		putchar(*s);
+		s--;
+	}
 
-		        int o;
-
-
-			        while (*s != '\0')
-
-					        {
-
-							                longi++;
-
-									                s++;
-
-											        }
-
-				        s--;
-
-					        for (o = longi; o > 0; o--)
-
-							        {
-
-									                _putchar(*s);
-
-											                s--;
-
-													        }
-
-
-						        _putchar('\n');
-
+	_putchar('\n');
 }
